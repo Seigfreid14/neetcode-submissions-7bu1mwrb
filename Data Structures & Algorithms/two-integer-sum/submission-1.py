0b33567cc86a3648e130f047e4824467ccccf43b
Seigfreid14 <dhruvbisht15.0008@gmail.com>
2026-03-31 20:16:18 +0530

    Update: two-integer-sum - submission-1

--- Data Structures & Algorithms/two-integer-sum/submission-1.py
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -1,4 +1,10 @@
 class Solution:
-  
-    def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if(nums[i]+nums[j]==target):
+                    return [i,j]
+                    
+
+        
